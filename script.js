52043e13947a8c267162a78c45ba21c28e307daa
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-15 21:54:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+function stripArticles(name) {
+    return name.replace(/^(a |an |the )/i, '');
+}
+
+touristSpots.sort((a, b) => stripArticles(a) > stripArticles(b) ? 1 : -1);
+
+let list = document.querySelector('#band');
+touristSpots.forEach(spot => {
+    let li = document.createElement('li');
+    li.textContent = spot;
+    list.appendChild(li);
+});
